b0eea680401c61c35080fbfee8f197e2476ae075
razakhalid <ra97za@gmail.com>
2024-04-11 17:10:52 -0500

    added preconnect font tags to head with priority 5

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@
 
 // includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
+include(get_theme_file_path('/includes/front/head.php'));
 
 // hooks
 add_action('wp_enqueue_scripts', 'u_enqueue');
+add_action('wp_head', 'u_head', 5);
--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -3,7 +3,9 @@
 function u_enqueue() {
     wp_register_style(
         'u_font_rubik_and_pacifico',
-        'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap'
+        'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
+        [],
+        null
     );
     wp_register_style(
         'u_bootstrap_icons',
@@ -13,7 +15,6 @@ function u_enqueue() {
         'u_theme',
         get_theme_file_uri('assets/public/index.css')
     );
-
 
     wp_enqueue_style('u_font_rubik_and_pacifico');
     wp_enqueue_style('u_bootstrap_icons');
