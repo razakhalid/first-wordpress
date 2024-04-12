5bc73ecc81105f8a955a4874661e86c423bfea44
razakhalid <ra97za@gmail.com>
2024-04-11 23:49:03 -0500

    created templates

--- functions.php
+++ functions.php
@@ -5,7 +5,9 @@
 // includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
 include(get_theme_file_path('/includes/front/head.php'));
+include(get_theme_file_path('/includes/setup.php'));
 
 // hooks
 add_action('wp_enqueue_scripts', 'u_enqueue');
 add_action('wp_head', 'u_head', 5);
+add_action('after_setup_theme', 'u_setup_theme');
